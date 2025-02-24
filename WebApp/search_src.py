7f4147a48d126bc56b62b2d89a75f085c6bc8a00
Artur m <arturm9816@gmail.com>
2025-02-24 16:42:50 +0000

    made profile pictures show up in search, and added no profile picture placeholder

--- WebApp/search_src.py
+++ WebApp/search_src.py
@@ -1,4 +1,5 @@
 from .models import User
+from django.db import connection
 
 
 # function to return true if datbase_username is to be returned
@@ -10,10 +11,24 @@ def _match(input_username, database_username):
         database_username in input_username
         
 
+
+
+def get_profile_picture_path_SQL(username):
+    return                                    \
+        "SELECT profile_picture "            + \
+        "FROM WebApp_user,"         + \
+        "     WebApp_profile "    + \
+        "WHERE WebApp_user.id = WebApp_profile.id "        + \
+       f"AND WebApp_user.username = '{username}'"
+
+
+
 class SearchResult:
     username = None
     link = None
     score = None
+    profile_picture_path = None
+    has_profile_pic = None
 
 # get list of names that match with input_username
 def searchForUsername(input_username):
@@ -23,15 +38,26 @@ def searchForUsername(input_username):
     database = User.objects.values_list;
     matches = []
 
-    for database_username in database("username"):
+    with connection.cursor() as SQL_reader:
 
-        if _match(input_username, database_username[0]):
+        for database_username in database("username"):
 
-            result = SearchResult()
-            result.username = database_username[0]
-            result.score = None # todo
-            result.link = f"../../profile/{result.username}"
+            if _match(input_username, database_username[0]):
+
+                result = SearchResult()
+                result.username = database_username[0]
+                result.score = None # todo
+                result.link = f"../../profile/{result.username}"
+
+                query = get_profile_picture_path_SQL(result.username)
+                SQL_reader.execute(query)
+                sql_result = SQL_reader.fetchall()
+
+                result.profile_picture_path = \
+                    None if sql_result == [('',)] else ("../WebApp/media/" + sql_result[0][0])
             
-            matches.append(result)
+                result.has_profile_pic = result.profile_picture_path is not None
+
+                matches.append(result)
 
     return matches;
